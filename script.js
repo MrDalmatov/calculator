9448071b97e8364b96478f74ad7406504c4cbc64
Родион Трынкин <Rodion@MacBookPro.local>
2023-11-04 17:08:08 +0700

    fixed bug with double characters

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ const display = document.querySelector('.calculator__display');
 const keys = document.querySelectorAll('.calculator__key');
 let resultCalculated = false;
 
-
 keys.forEach(key => {
   key.addEventListener('click', () => {
     const value = key.textContent;
+    const lastChar = display.textContent.slice(-1);
 
     if (resultCalculated) {
       display.textContent = '';
@@ -18,8 +18,16 @@ keys.forEach(key => {
       display.textContent = eval(display.textContent);
       resultCalculated = true;
     } else {
-      display.textContent += value;
+      if (
+        (lastChar === '+' || lastChar === '-' || lastChar === '*' || lastChar === '/') &&
+        (value === '+' || value === '-' || value === '*' || value === '/')
+      ) {
+        return;
+      } else if (display.textContent === '0' || display.textContent === 'Infinity' || display.textContent === '-Infinity') {
+        display.textContent = value;
+      } else {
+        display.textContent += value;
+      }
     }
-
   });
 });
