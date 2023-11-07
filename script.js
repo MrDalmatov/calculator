f86a0ad6ac27d28df584e918a13f5baadc7be1da
Родион Трынкин <Rodion@MacBookPro.local>
2023-11-07 16:57:23 +0700

    fixed decimal problems

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const display = document.querySelector('.calculator__display');
 const keys = document.querySelectorAll('.calculator__key');
 let resultCalculated = false;
+let decimalEntered = false;
 
 keys.forEach(key => {
   key.addEventListener('click', () => {
@@ -14,16 +15,23 @@ keys.forEach(key => {
 
     if (value === 'C') {
       display.textContent = '';
+      decimalEntered = false;
     } else if (value === '=') {
       display.textContent = eval(display.textContent);
       resultCalculated = true;
     } else {
       if (
-        (lastChar === '+' || lastChar === '-' || lastChar === '*' || lastChar === '/') &&
-        (value === '+' || value === '-' || value === '*' || value === '/')
+        (lastChar === '+' || lastChar === '-' || lastChar === '*' || lastChar === '/' || lastChar === '.') &&
+        (value === '+' || value === '-' || value === '*' || value === '/' || value === '.')
       ) {
         return;
-      } else if (display.textContent === '0' || display.textContent === 'Infinity' || display.textContent === '-Infinity') {
+      } else if (value === '.' && decimalEntered) {
+        return;
+      } else if (value === '.') {
+        decimalEntered = true;
+      }
+      
+      if (display.textContent === '0' || display.textContent === 'Infinity' || display.textContent === '-Infinity') {
         display.textContent = value;
       } else {
         display.textContent += value;
